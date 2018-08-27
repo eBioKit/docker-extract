3f3420a4e46a02040ebb6c325d733494b27f0845
fikipollo <rafahd87@gmail.com>
2018-08-27 21:50:51 +0200

    remove tool

--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -4,6 +4,9 @@ COMMAND=$1
 if [[ "$COMMAND" == "extract" ]];then
   COMPRESSED=$2
   tar --same-owner -I pbzip2 -xvf /in/${COMPRESSED} -C /out/
+elif [[ "$COMMAND" == "remove" ]];then
+    TARGET=$2
+    ls /in/$TARGET
 elif [[ "$COMMAND" == "bash" ]];then
   /bin/bash
 fi
